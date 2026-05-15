0c0603276754520bf33e7e7b9d263e5265c751aa
Saurabh Kumar <patiramproduction@gmail.com>
2026-05-15 14:47:55 +0530

    jwt not working

--- application/helpers/jwt_helper.php
+++ application/helpers/jwt_helper.php
@@ -1,36 +1,30 @@
 <?php
+defined('BASEPATH') or exit('No direct script access allowed');
 
 use Firebase\JWT\JWT;
 use Firebase\JWT\Key;
 
 if (!function_exists('generate_jwt')) {
-
-    function generate_jwt($data)
+    function generate_jwt($user_data)
     {
-        $key = "S@urabh#JWT$" . 'SecretKey_2026_' . "!Secure123456";
+        // 🔐 Change this to a random, complex string. Keep it secret!
+        $secret_key = "Your_Super_Secret_Billing_App_Key_2026"; 
+        
+        $issued_at = time();
+        $expire_time = $issued_at + (60 * 60 * 24); // Token valid for 24 hours
 
-        $payload = [
-            'iss' => "localhost",
-            'aud' => "localhost",
-            'iat' => time(),
-            'exp' => time() + (60 * 60), // 1 hour
-            'data' => $data
-        ];
+        $payload = array(
+            'iss'  => base_url(),         // Issuer
+            'iat'  => $issued_at,         // Issued at
+            'exp'  => $expire_time,        // Expiration time
+            'data' => [
+                'user_id'  => $user_data['user_id'],
+                'email'    => $user_data['email'],
+                'username' => $user_data['username']
+            ]
+        );
 
-        return JWT::encode($payload, $key, 'HS256');
-    }
-}
-
-if (!function_exists('verify_jwt')) {
-
-    function verify_jwt($token)
-    {
-        $key = "your_secret_key";
-
-        try {
-            return JWT::decode($token, new Key($key, 'HS256'));
-        } catch (Exception $e) {
-            return false;
-        }
+        // Encode the payload into a JWT token using HS256 algorithm
+        return JWT::encode($payload, $secret_key, 'HS256');
     }
 }
